a5b24f2244bb360eda528ed3591c678a36b9b893
linda <linda@lindas-MacBook-Air.local>
2021-07-23 10:36:38 +0100

    completed

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
 
-io.on('connection', socket => { 
-    console.log()
-})
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+app.use(cors());
 
-server.listen(6000, () => {
-
-    console.log(`server is running on port 6000......`)
+io.on("connection", (socket) => {
+  console.log("connection made successfully");
+  socket.on("message", (payload) => {
+    console.log("message received on server: ", payload);
+    io.emit("message", payload);
+  });
 });
 
+server.listen(7000, () => {
+  console.log(`server is running on port 7000......`);
+});
